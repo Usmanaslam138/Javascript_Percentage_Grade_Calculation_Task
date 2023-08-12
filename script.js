3a8874a41e8f41514823fbfd2e390ac19c91d5a7
Usman Digitaldev <usmanaslamm138@gmail.com>
2023-08-12 17:22:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ let heading1 = document.getElementById('heading1')
 let paragraph1 = document.getElementById('paragraph1')
 let paragraph2 = document.getElementById('paragraph2')
 
-let marksC = parseInt(prompt("Enter the marks of chemistry "))
-let marksP = parseInt(prompt("Enter the marks of physics "))
-let marksM = parseInt(prompt("Enter the marks of maths"))
-let marksE = parseInt(prompt("Enter the marks of english "))
-let marksB = parseInt(prompt("Enter the marks of biology "))
+var marksC = parseInt(prompt("Enter the marks of chemistry "))
+var marksP = parseInt(prompt("Enter the marks of physics "))
+var marksM = parseInt(prompt("Enter the marks of maths"))
+var marksE = parseInt(prompt("Enter the marks of english "))
+var marksB = parseInt(prompt("Enter the marks of biology "))
 
-if (marksC == NaN) {
-    alert("You must have to enter all subjects marks")
+if (isNaN(marksC) || isNaN(marksP) || isNaN(marksM) || isNaN(marksE) || isNaN(marksB)) {
+    alert("you must have to enter all subjects marks")
 }
 else if (marksC > 100 || marksP > 100 || marksM > 100 || marksE > 100 || marksB > 100) {
     alert("Marks should be less than or equal to 100 ")
@@ -38,8 +38,9 @@ else {
     else {
         Grade = "F"
     }
+
     heading1.innerHTML = "Result"
     paragraph1.innerHTML = `Percentage = ${percentage}`
     paragraph2.innerHTML = `Grade = ${Grade}`
-}
 
+}
